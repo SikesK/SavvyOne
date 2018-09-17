a12b59def613032b7f03dec2c1a0a3e9b9248295
Kelsey Sikes <KelseySikes@Kelseys-MacBook-Pro-2.local>
2018-09-17 18:19:45 -0500

    updates

--- store/index.js
+++ store/index.js
@@ -3,3 +3,4 @@ export { default as home } from './Home';
 export { default as archive } from './archive';
 export { default as thecache } from './thecache';
 export { default as about } from './about';
+export { default as Posts } from './Posts'; // added
